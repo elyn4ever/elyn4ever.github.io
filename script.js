f1ed7fd897c3c18b8d0a6b5ad17805ffbed58722
elyn4ever <164569969+elyn4ever@users.noreply.github.com>
2024-03-22 22:39:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const products = [
-        { id: 1, name: 'Product 1', price: 10, description: 'Description for Product 1', image: '1.jpg' },
-        { id: 2, name: 'Product 2', price: 20, description: 'Description for Product 2', image: '2.jpg' },
-        { id: 3, name: 'Product 3', price: 30, description: 'Description for Product 3', image: '3.jpg' },
-        { id: 4, name: 'Product 4', price: 40, description: 'Description for Product 4', image: '4.jpg' },
-        { id: 5, name: 'Product 5', price: 50, description: 'Description for Product 5', image: '5.jpg' },
-        { id: 6, name: 'Product 6', price: 60, description: 'Description for Product 6', image: '6.jpg' },
+        { id: 1, name: 'Tee LS, T-shirt - Off White', price: 10, description: 'T-shirt with long sleeves and buttons. High quality and good fit.', image: '1.jpg' },
+        { id: 2, name: 'Tee LS, T-shirt - Rose', price: 279, description: 'T-shirt with long sleeves and buttons. High quality and good fit.', image: '2.jpg' },
+        { id: 3, name: 'Tee LS, T-shirt - Pale Blue', price: 379, description: ''T-shirt with long sleeves and buttons. High quality and good fit.', image: '3.jpg' },
+        { id: 4, name: 'Tee LS, T-shirt - Blue', price: 479, description: ''T-shirt with long sleeves and buttons. High quality and good fit.', image: '4.jpg' },
+        { id: 5, name: 'Tee LS, T-shirt - Grey Melange', price: 579, description: ''T-shirt with long sleeves and buttons. High quality and good fit.', image: '5.jpg' },
+        { id: 6, name: 'Plain Body LS, Bodystocking - Rose/Grey Melange', price: 179, description: 'Body with long sleeves in a soft quality with a good fit. Pushbuttons for easy changing.', image: '6.jpg' },
 
     ];
     const cart = [];
